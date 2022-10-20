7c4247c31550653a3803b7fa6feb0c5841090bf4
Jax Morgan <jmorga@uw.edu>
2022-10-20 14:27:40 -0700

    setting blank home page

--- project/src/home.js
+++ project/src/home.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
-import { JobList } from './Jobcards.js';
-import JobSelectForm from './Search.js';
 
 export function Home(props) {
 
@@ -9,17 +6,17 @@ export function Home(props) {
     <div>
       <header className="header-with-image">
         <div>
-          <h1 className="big-font">Search. Track. Build.</h1>
-          <p className="small-font">WorkCTRL is the number one platform to search for job opportunities and track jobs applied to while building community with others. This platform personalizes your career options to align with your interests so you can meet your goals.</p>
+          <h1 className="big-font">x.</h1>
+          <p className="small-font">X.</p>
         </div>
       </header>
 
       <main>
         <section>
           <div className="job-help">
-            <div className="item"><a href="https://careers.uw.edu/channels/resumes/" target="blank">Resume</a></div>
-            <div className="item"><a href="https://careers.uw.edu/channels/cover-letters/" target="blank">Cover Letter</a></div>
-            <div className="item"><a href="https://www.linkedin.com/" target="blank">LinkedIn</a></div>
+            <div className="item"><a href="" target="blank">X</a></div>
+            <div className="item"><a href="" target="blank">Y</a></div>
+            <div className="item"><a href="" target="blank">Z</a></div>
           </div>
         </section>
       </main>
