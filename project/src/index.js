112bb5869ec9e70d2adc5c7294e0e19e61571e07
Jax Morgan <jmorga@uw.edu>
2022-10-25 13:55:42 -0700

    disregard

--- project/src/index.js
+++ project/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import NavBar from './components/NavBar'
+import NavBar from './components/Navbar'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
