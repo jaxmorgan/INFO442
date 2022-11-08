72e9735616c858794986d9a5fa98d91ff467dcfc
Jax Morgan <jmorga@uw.edu>
2022-11-07 20:22:19 -0800

    B Corp Certification Link

--- project/src/components/About/about.js
+++ project/src/components/About/about.js
@@ -20,7 +20,7 @@ return (
             <p><a className="about-categories" href="https://wedesigngreen.com/stories/__trashed">Overall climate risk</a></p>
           </ul>
           <p>Many of our companies are B Corp Certified. What does this mean? </p>
-          <p>According to https://www.bcorporation.net/en-us/certification a B Corp Certification is a designation that a business is meeting high standards of verified performance, accountability, and transparency on factors from employee benefits and charitable giving to supply chain practices and input materials. In order to achieve certification, a company must: </p>
+          <p>A B Corp Certification is a designation that a business is meeting high standards of verified performance, accountability, and transparency on factors from employee benefits and charitable giving to supply chain practices and input materials. In order to achieve certification, a company must: </p>
           <ul>
             <li>Demonstrate high social and environmental performance by achieving a B Impact Assessment score of 80 or above and passing their risk review. Multinational corporations must also meet baseline requirement standards. </li>
             <li>Make a legal commitment by changing their corporate governance structure to be accountable to all stakeholders, not just shareholders, and achieve benefit corporation status if available in their jurisdiction. </li>
