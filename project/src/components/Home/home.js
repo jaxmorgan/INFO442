fd7ac1f16e0d3852535e13ea4bdd18771737e1cf
Jax Morgan <jmorga@uw.edu>
2022-11-10 15:30:19 -0800

    home colors

--- project/src/components/Home/home.js
+++ project/src/components/Home/home.js
@@ -8,9 +8,8 @@ export default function Home(props) {
   return (
     <main>
       <header className="column-container">
-          <div class="text-block">
-            <div className="welcome-to-source"><a href="/About">Welcome to Source</a></div>
-          </div>
+          <div class="text-block"></div>
+            <div><p className="shop-sustainably"><span>Shop. Sustainably.</span></p></div>
       </header>
   
   <div class="two-col-container">
