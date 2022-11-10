f550195efa82e9a5ba6ca8d8932ad6d60a605962
Jax Morgan <jmorga@uw.edu>
2022-11-10 13:28:05 -0800

    Ongoing about edits

--- project/src/components/About/about.js
+++ project/src/components/About/about.js
@@ -8,7 +8,7 @@ return (
         <div className="about-content">
           <header className="column-container-about">
           <div class="text-block">
-            <div className="welcome-to-source"><a href="/About">Shop Source</a></div>
+            <div className="welcome-to-source"><a href="/Shop">Shop Source</a></div>
           </div>
       </header>
 
@@ -17,7 +17,7 @@ return (
         <div class="col-6 border-right">
           <div class="col-sm">
           <p className="small-font">Source is on a mission to connect you with sustainable businesses for all of your home goods needs. We are committed to improving the current state of e-commerce through eliminating unsustainable consumer habits and mass production practices.</p>
-          <p className="small-font-2">Our supplies are manually verified businesses who meet our minimum threshold requirements for sustainability. So, what do we look at specifically when it comes to our products and suppliers?:</p>
+          <p className="small-font">Our supplies are manually verified businesses who meet our minimum threshold requirements for sustainability. So, what do we look at specifically when it comes to our products and suppliers?:</p>
           <ul>
             <p><a className="about-categories" href="https://www.epa.gov/sustainability/sustainable-manufacturing#:~:text=Sustainable%20manufacturing%20is%20the%20creation,employee%2C%20community%20and%20product%20safety.">Manufacturing processes</a></p>
             <p><a className="about-categories" href="https://www.epa.gov/ghgemissions">Greenhouse gas emissions</a></p>
@@ -32,9 +32,9 @@ return (
           <p className="small-font">Many of our companies are B Corp Certified. What does this mean? </p>
           <p className="small-font">A B Corp Certification is a designation that a business is meeting high standards of verified performance, accountability, and transparency on factors from employee benefits and charitable giving to supply chain practices and input materials. In order to achieve certification, a company must: </p>
           <ul>
-            <li><span>Demonstrate high social and environmental performance by achieving a B Impact Assessment score of 80 or above and passing their risk review. Multinational corporations must also meet baseline requirement standards.</span></li>
-            <li><span>Make a legal commitment by changing their corporate governance structure to be accountable to all stakeholders, not just shareholders, and achieve benefit corporation status if available in their jurisdiction.</span></li>
-            <li><span>Exhibit transparency by allowing information about their performance measured against B Lab’s standards to be publicly available on their B Corp profile on B Lab’s website.</span></li>
+            <li className="small-font"><span>Demonstrate high social and environmental performance by achieving a B Impact Assessment score of 80 or above and passing their risk review. Multinational corporations must also meet baseline requirement standards.</span></li>
+            <li className="small-font"><span>Make a legal commitment by changing their corporate governance structure to be accountable to all stakeholders, not just shareholders, and achieve benefit corporation status if available in their jurisdiction.</span></li>
+            <li className="small-font"><span>Exhibit transparency by allowing information about their performance measured against B Lab’s standards to be publicly available on their B Corp profile on B Lab’s website.</span></li>
           </ul>
           <p className="small-font">When you shop on Source, you can be assured that you’re making purchases that are good for the planet.</p>
           </div>
