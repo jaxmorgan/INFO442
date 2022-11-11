2836b8c1cc298b3ede2c419d923188013cc90170
Shockiiii <77355894+Shockiiii@users.noreply.github.com>
2022-11-10 20:25:11 -0800

    filter update

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, useSearchParams  } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, useSearchParams } from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar/navbar.js';
 import About from './components/About/about.js';
 import Home from './components/Home/home.js';
 import Account from './components/Account/account.js';
 import Items from './components/Items/items';
-import { ItemsContent } from "./data/productsData";
+import { ItemsContent } from './data/productsData';
 import { useState } from 'react';
 
 
@@ -28,7 +28,7 @@ export default function App(props) {
   const [cart, setCart] = useState()
 
   const handleClick = (item) => {
-    
+
     // Add to cart
     setCart((cart) => [
       ...cart,
@@ -37,21 +37,26 @@ export default function App(props) {
   };
 
 
+  
+
+ 
+
+
 
   return (
     <div>
       <Router>
         <NavBar />
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="/Home" element={<Home />} />
+          <Route index element={<Home/>} />
+          <Route path="/Home" element={<Home/>} />
           <Route path="/" render={() => <Navigate to="/Home" />} />
           <Route path="/About" element={<About />} />
-          <Route path="/Shop" element={<Home />} />
+          <Route path="/Shop" element={<Home/>} />
           <Route path="/Account" element={<Account />} />
-        </Routes>        
+        </Routes>
       </Router>
-
+      
     </div>
   );
 }
--- project/src/components/Home/home.js
+++ project/src/components/Home/home.js
@@ -2,8 +2,38 @@ import React from 'react';
 import Items from '../Items/items';
 import Item from '../Items/items';
 import SideFilter from './sidefilter';
+import { ItemsContent } from '../../data/productsData';
+import { useState } from 'react';
 
 export default function Home(props) {
+
+
+  const hookArray = useState({type: 'Show all items', supplier: 'Show all items'});
+  const filter = hookArray[0];
+  const setFilter = hookArray[1];
+
+  
+
+  let displayedData;
+
+  if (filter.type === "Show all items") {
+    displayedData = ItemsContent;
+  } else {
+    displayedData = ItemsContent.filter(
+      (n) => {
+        return n.type === filter.type;
+      }
+    );
+  }
+
+  const applyFilter = (filter1, filter2) => {
+    setFilter({ type: filter1, supplier: filter2 });
+}
+
+
+
+
+
 
   return (
     <main>
@@ -13,8 +43,8 @@ export default function Home(props) {
       </header>
   
   <div class="two-col-container">
-    <SideFilter />
-    <Items />
+    <SideFilter applyFilterCallback={applyFilter}/>
+    <Items data={displayedData}/>
   </div>
 
       <footer>
