9fc670077c4207fe9c4f5636732b41178a78a79f
Jax Morgan <jmorga@uw.edu>
2022-11-11 21:06:37 -0800

    pushing firebase + account page changes

--- project/src/components/Account/account.js
+++ project/src/components/Account/account.js
@@ -18,15 +18,15 @@ export default function Account(props) {
                 <div class="d-flex justify-content-between align-items-center">
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">First Name</label><input type="text" class="form-control" placeholder="first name" value="" /></div>
-                    <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" value="" placeholder="last name" /></div>
+                    <div class="col-md-6"><label class="labels">First Name</label><input type="text" class="form-control form-control-lg" placeholder="first name" value="" /></div>
+                    <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control form-control-lg" value="" placeholder="last name" /></div>
                 </div>
                 <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value="" /></div>
-                    <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                    <div class="col-md-12"><label class="labels">Zip Code</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                    <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value="" /></div>
-                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value="" /></div>
+                    <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 1" value="" /></div>
+                    <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 2" value="" /></div>
+                    <div class="col-md-12"><label class="labels">Zip Code</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 2" value="" /></div>
+                    <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 2" value="" /></div>
+                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control form-control-lg" placeholder="enter email id" value="" /></div>
                 </div>
                 <div class="button-text"><button class="account-btn" type="button">Save Profile</button></div>
             </div>
