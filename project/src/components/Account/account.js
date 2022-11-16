857543ace334953b570c30054af40324fd520e37
Jax Morgan <jmorga@uw.edu>
2022-11-16 13:21:42 -0800

    form inputs ip

--- project/src/components/Account/account.js
+++ project/src/components/Account/account.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 
 export default function Account(props) {
-    return (
 
+    const [value, setValue] = useState("");
+
+const handleChange = (e) => {
+    setValue(e.target.value);
+    localStorage.setItem("inputValue", e.target.value);
+  };
+
+  useEffect(() => {
+    setValue(localStorage.getItem("inputValue"));
+  }, []);
+  
+    return (
     <div className="account-background">
           <header className="column-container-account">
           <div class="text-block">
@@ -16,15 +28,15 @@ export default function Account(props) {
                 <div class="d-flex justify-content-between align-items-center">
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">First Name</label><input type="text" class="form-control form-control-lg" placeholder="first name" value="" /></div>
-                    <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control form-control-lg" value="" placeholder="last name" /></div>
+                    <div class="col-md-6"><label class="labels">First Name</label><input value={value} onChange={handleChange} type="text-1" class="form-control form-control-lg" placeholder="first name" /></div>
+                    <div class="col-md-6"><label class="labels">Last Name</label><input value={value} onChange={handleChange} type="text-2" class="form-control form-control-lg" placeholder="last name" /></div>
                 </div>
                 <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 1" value="" /></div>
-                    <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 2" value="" /></div>
-                    <div class="col-md-12"><label class="labels">Zip Code</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 2" value="" /></div>
-                    <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control form-control-lg" placeholder="enter address line 2" value="" /></div>
-                    <div class="col-md-12"><label class="labels">Email</label><input type="text" class="form-control form-control-lg" placeholder="enter email" value="" /></div>
+                    <div class="col-md-12"><label class="labels">Address Line 1</label><input value={value} onChange={handleChange} type="text" class="form-control form-control-lg" placeholder="enter address line 1" /></div>
+                    <div class="col-md-12"><label class="labels">Address Line 2</label><input value={value} onChange={handleChange} type="text" class="form-control form-control-lg" placeholder="enter address line 2"/></div>
+                    <div class="col-md-12"><label class="labels">Zip Code</label><input value={value} onChange={handleChange} type="text" class="form-control form-control-lg" placeholder="enter address line 2"/></div>
+                    <div class="col-md-12"><label class="labels">State</label><input value={value} onChange={handleChange} type="text" class="form-control form-control-lg" placeholder="enter address line 2"  /></div>
+                    <div class="col-md-12"><label class="labels">Email</label><input value={value} onChange={handleChange} type="text" class="form-control form-control-lg" placeholder="enter email" /></div>
                 </div>
                 <div class="button-text"><button class="account-btn" type="button">Save Profile</button></div>
             </div>
--- project/src/components/Cart/cart.js
+++ project/src/components/Cart/cart.js
@@ -17,14 +17,14 @@ export default function Cart(props) {
                         <div className="d-flex justify-content-between align-items-center">
                         </div>
                         <div className="row mt-3">
-                            <div className="col-md-12"><label className="labels">Cardholder's Name</label><input type="text" className="form-control form-control-lg" placeholder="cardholder's name" value="" /></div>
-                            <div className="col-md-12"><label className="labels">Shipping Address</label><input type="text" className="form-control form-control-lg" placeholder="shipping address" value="" /></div>
-                            <div className="col-md-12"><label className="labels">Card Number</label><input type="text" className="form-control form-control-lg" placeholder="card number" value="" /></div>
-                            <div className="col-md-12"><label className="labels">Billing Address</label><input type="text" className="form-control form-control-lg" placeholder="billing address" value="" /></div>
+                            <div className="col-md-12"><label className="labels">Cardholder's Name</label><input type="text" className="form-control form-control-lg" placeholder="cardholder's name"/></div>
+                            <div className="col-md-12"><label className="labels">Shipping Address</label><input type="text" className="form-control form-control-lg" placeholder="shipping address"/></div>
+                            <div className="col-md-12"><label className="labels">Card Number</label><input type="text" className="form-control form-control-lg" placeholder="card number"/></div>
+                            <div className="col-md-12"><label className="labels">Billing Address</label><input type="text" className="form-control form-control-lg" placeholder="billing address"/></div>
                         </div>
                         <div className="row mt-2">
-                            <div className="col-md-6"><label className="labels">Expiration</label><input type="text" className="form-control form-control-lg" placeholder="expiration" value="" /></div>
-                            <div className="col-md-6"><label className="labels">CVV</label><input type="text" className="form-control form-control-lg" value="" placeholder="cvv" /></div>
+                            <div className="col-md-6"><label className="labels">Expiration</label><input type="text" className="form-control form-control-lg" placeholder="expiration" /></div>
+                            <div className="col-md-6"><label className="labels">CVV</label><input type="text" className="form-control form-control-lg" placeholder="cvv" /></div>
                         </div>
                         <div className="button-text"><button className="btn btn-primary" type="button">Purchase</button></div>
                     </div>
