65abe6d764861dfc8031239040db2c082e0c9166
jzenguw <justinzeng007@gmail.com>
2022-12-01 23:36:37 -0800

    feat: cart in home page working real time and displays right cards

--- project/src/components/Home/home.js
+++ project/src/components/Home/home.js
@@ -149,32 +149,19 @@ export default function Home(props) {
 
 
 
-  const [cart, setCart] = useState([
+  const [cart, setCart] = useState([]);
 
-    {"name":"Penn Chair", 
-    "type":"chair", 
-    "price":"$174.99",
-    "range":"low",
-    "delivery":"1-2 days",
-    "img":"./img/penn-chair-o.jpg", 
-    "status": "new",
-    "id":"1", 
-    "supplier":"West Elm", 
-    "supplierLink":"https://www.westelm.com/pages/about-west-elm/?cm_re=sitewidebanner-_-default-_-Sustainability", 
-    "addedToCart":"No"}
-
-
-  ]);
-
-
+  useEffect(() => {
+    // Update the document title using the browser API
+    setCart(cart)
+  }, [cart]);
   const addToCart = (product) => {  
     //setCart([...cart, product]);
     const newCart = [...cart];
     newCart.push(product);
     setCart(newCart);
 
-
-    console.log(product);
+    console.log(cart);
 
   };
 
